d709e23d95bca01ea2e10bb37ecc7f3dcd7d641b
Thu Van <luuluu_1975@yahoo.com.au>
2021-06-07 21:15:01 +1000

    update DB settings

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
 var mysql = require('mysql');
 
-var connection;
 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL)
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
-
-	connection = mysql.createConnection({
-	  host     : 'localhost',
-	  user     : 'root',
-	  password : 'yourrootpassword',
-	  database : 'burgers_db'
+	var connection = mysql.createConnection({
+	host: 'localhost',
+	port:'3306',	
+	user:'root',
+	password:'yourrootpassword',
+	database:'burgers_db'
 	});
 }
-
 
 connection.connect(function (err) {
 	if (err) {
